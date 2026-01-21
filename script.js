ac1151df213483eb1cd47fba60ae817727e82163
Juuhz <julianeleite16@gmail.com>
2026-01-21 15:27:15 -0300

    inclusao js

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("formCadastro");
+const btnSubmit = document.getElementById("btnSubmit");
+
+/* SUCESSO */
+const inputEmail = document.getElementById("email");
+const inputSenha = document.getElementById("senha");
+
+/* REGEX EMAIL */
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+/* VALIDAÇÃO */
+function validarForm(){
+    let estaOk = true;
+}
+
+/* VAL 1 - TESTAR EMAIL */
+
+
+/* VAL 2 - TESTAR SENHA */
+
